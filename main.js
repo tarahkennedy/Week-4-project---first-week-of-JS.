9e380c6ae7674494cbd1e7b484aca101147e22b3
tarahkennedy <tarahek@yahoo.ca>
2015-08-04 11:37:40 -0400

    Week 4 project - first week of JavaScript

---
+++ main.js
@@ -0,0 +1,43 @@
+// HackerYou Week 4 Project
+// By: Christina Fung, Tarah Kennedy
+
+$('.flower1').on('click', function(){
+	$(".coverBox1").fadeIn("slow").removeClass("hide");
+
+});
+
+$('.flower2').on('click', function(){
+	$(".coverBox2").fadeIn("slow").removeClass("hide");
+});
+
+$('.flower3').on('click', function(){
+	$(".coverBox3").fadeIn("slow").removeClass("hide");
+});
+
+$('.flower4').on('click', function(){
+	$(".coverBox4").fadeIn("slow").removeClass("hide");
+});
+
+$('.flower5').on('click', function(){
+	$(".coverBox5").fadeIn("slow").removeClass("hide");
+});
+
+$('.flower6').on('click', function(){
+	$(".coverBox6").fadeIn("slow").removeClass("hide");
+});
+
+$('.flower7').on('click', function(){
+	$(".coverBox7").fadeIn("slow").removeClass("hide");
+});
+
+$('.flower8').on('click', function(){
+	$(".coverBox8").fadeIn("slow").removeClass("hide");
+});
+
+$('.flower9').on('click', function(){
+	$(".coverBox9").fadeIn("slow").removeClass("hide");
+});
+
+$("i").on("click", function() {
+	$(".coverBox").fadeOut("slow").addClass("hide");
+})
